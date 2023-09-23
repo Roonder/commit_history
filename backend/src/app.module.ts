30c8039c416a1bbfd45a000e11a4f1bf58f1fc52
roonder <apontejuliam@gmail.com>
2023-09-23 11:27:35 -0400

    [update]: Installing @nestjs/config dependency and adjusting CORS for Frontend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GithubController } from './app.controller';
 import { GithubService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [GithubController],
   providers: [GithubService],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log(process.env.FRONTEND_URL);
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: ['GET'],
+  });
   await app.listen(3000);
 }
 bootstrap();
