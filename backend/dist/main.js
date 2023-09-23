e73dda9abdec254552bca689ca32250a36252788
roonder <apontejuliam@gmail.com>
2023-09-23 11:28:18 -0400

    [add]: Adding dist folder for Nest.js config dependency

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -4,6 +4,13 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    console.log(process.env.FRONTEND_URL);
+    app.enableCors({
+        origin: process.env.FRONTEND_URL,
+        credentials: true,
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        methods: ['GET'],
+    });
     await app.listen(3000);
 }
 bootstrap();
