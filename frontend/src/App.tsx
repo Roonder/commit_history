bfb910865b6f43c72153cb61235ebb86366d922a
roonder <apontejuliam@gmail.com>
2023-09-23 21:32:31 -0400

    [styles]: Dashboard styles adjusted - Aligning components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <h1 className="flex items-center gap-2 text-3xl justify-center font-bold"><FaGithub/> GitHub</h1>
       <h2 className="font-semibold text-center text-xl mb-2">Commits</h2>
 
-      <div className="md:flex md:justify-around md:items-center">
+      <div className="md:flex md:justify-around">
         <RepositoryInfoLoader/>
         <CommitsLoader/>
       </div>
